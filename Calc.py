82e1139d4cfc2f0947e8736d460a845bcfdbb476
Hacker182008 <aryanp1027@gmail.com>
2026-06-18 17:06:50 +0530

    Added calculator program

---
+++ Calc.py
@@ -0,0 +1,25 @@
+# This program of print all
+a = int(input("Enter your number"))
+b = int(input("Enter your number"))
+
+
+# Calculation
+
+c = a+b
+d = a*b
+e = a**b
+f = a/b
+g = a//b
+h = a%b
+i = a-b
+
+
+# Printing results
+
+print("Addition is",c)
+print("Multiplication is",d)
+print(" Division is",e)        
+print("Floor division is",g)
+print("Modulus is",h)                          
+print(" division is",f)
+print("Subraction is",i)              
